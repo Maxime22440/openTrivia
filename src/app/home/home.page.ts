95d4a78e2548ee4940d67a839f5a5ee80e8f8558
Maxime ROUSSEAU <maxime.rousseau2022@campus-eni.fr>
2023-10-03 10:17:20 +0200

    Mon premier commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pseudo: string = '';
+  difficulty: string = 'Easy';
+  listDifficulties: string[] = ['Easy', 'Medium', 'Hard'];
+  saveInfos: boolean = false;
+  nextQuestion: boolean = false;
+  begin: boolean = false;
 
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController) {}
+
+  async beginGame() {
+    if (this.pseudo.length < 3) {
+      const alert = await this.alertCtrl.create({
+        header: 'Information Manquante',
+        message: 'Veuillez rentrer un pseudo de 3 caractères minimum.',
+        buttons: ['OK']
+      });
+      alert.present();
+    } else {
+      this.begin = true;
+    }
+  }
+
+  checkAnswer(answer: string) {
+    this.showToast(answer);
+    this.nextQuestion = true;
+  }
+
+  async showToast(text: string) {
+    const toast = await this.toastCtrl.create({
+      message: text,
+      position: 'bottom',
+      duration: 4000
+    });
+    toast.present();
+  }
 }
